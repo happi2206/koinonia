659696149932da6971669668ade0f2ae5ff98fa8
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2022-02-15 10:08:02 +0100

    add: middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,9 @@
-import { GetterTree, ActionTree, MutationTree } from 'vuex'
+export default function ({ app, redirect }) {
+    if (
+        app.$localStorage.get('KOINONIA-TOKEN') === undefined ||
+        app.$localStorage.get('KOINONIA-LOGIN-STATE') === undefined
+    ) {
+        app.$localStorage.removeAll()
+        redirect('/login')
+    }
+}
