28e0d0fc5e0f050d4325fe3d78f4450ba49ec761
Happi <[diberrdigital@gmail.com]>
2022-02-18 13:11:49 +0100

    togglebtn

--- components/eventsHeaderCard.vue
+++ components/eventsHeaderCard.vue
@@ -36,7 +36,7 @@
     </div>
     <div class="bg-white rounded p-3 my-2">
       <filter-component>
-        <template #state="{ data }">
+        <template #status="{ data }">
           <toggleButton :value="data.value === 'status' ? true : false" />
         </template>
         <template #default="{ visualization }">
@@ -74,9 +74,8 @@ export default {
       studentsTable: [],
       test: 'hey',
       fields: [
-        { key: 'name', label: 'Surname', sortable: true },
-        { key: 'start_date', label: 'Other Name', sortable: true },
-        { key: 'end_date', label: 'Gender', sortable: true },
+        { key: 'other_name', label: 'Other Name', sortable: true },
+        { key: 'surname', label: 'Surname', sortable: true },
         { key: 'check_in', label: 'date/Time in', sortable: true },
         { key: '', label: 'Check in Method', sortable: true },
         { key: 'status', label: 'Status', sortable: true },
@@ -90,9 +89,24 @@ export default {
       const student = await this.$axios.$get(
         `course-v/get-all-students-in-an-event?course_id=${this.courseid}&event_id=${this.eventid}&page=1&size=50`
       )
-      const temp = []
-      temp.push(...student.items)
-      temp.forEach((el) => this.studentsInCourse.push(el))
+
+      //   console.log(...student.items)
+      //   this.studentsInCourse.push(...student.items)
+
+      const obj = student
+      obj.items.map((el) => {
+        console.log('el', el)
+        this.studentsInCourse.push({
+          status: el.status,
+          by: el.by,
+          check_in: el.check_in,
+          other_name: el.student.other_name,
+          surname: el.student.surname,
+          email: el.student.email,
+          id: el.student.id,
+        })
+      })
+      console.log('ob is', obj)
 
       console.log(this.studentsInCourse)
     } catch (e) {
