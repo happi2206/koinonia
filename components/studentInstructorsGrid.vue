90b07d1c853a1d6cb3bae286aa36be680f48e9f0
happi2206 <happinessadeboye@gmail.com>
2022-02-22 14:53:29 +0100

    grid view skeleton

--- components/studentInstructorsGrid.vue
+++ components/studentInstructorsGrid.vue
@@ -1,9 +1,13 @@
 <template>
-  <div
-    class="col-lg-4 col-6 my-3"
-    @grid-clicked="$emit('grid-clicked', $event)"
-  >
-    {{ data }}
+  <div class="col-md-4 my-3" @grid-clicked="$emit('grid-clicked', $event)">
+    <div v-for="(item, index) in data" :key="index">
+      <div class="my-4">
+        <p class="medbrownparagraph text-center">{{ item.email }}</p>
+        <p class="medbrownparagraph text-center">
+          {{ item.surname }} {{ item.other_name }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
