49930ae3d4dd9c78891b79d1538e2ee1e966e607
happi2206 <happinessadeboye@gmail.com>
2022-02-23 11:22:55 +0100

    form

--- pages/checkIn/course/_course/event/_event/index.vue
+++ pages/checkIn/course/_course/event/_event/index.vue
@@ -1,91 +1,68 @@
 <template>
-
   <div>
-        <login-nav />
-    <div class="mt-5 pt-md-5 horizontalspacing">
-      <div class="mt-5">
-        <h2 class="largebrownparagraph">Account</h2>
-      </div>
+    <login-nav />
+    <div class="horizontalspacing pt-5">
+      <div>
+        <div class="mt-5 pt-5">
+          <div class="pt-5">
+            <h2 class="text-center largebrownparagraph">
+              Welcome to your <span></span> course. Please fill in your details
+              accordingly
+            </h2>
+          </div>
+          <h2 class="largebrownparagraph text-center">Check in</h2>
+          <p class="medbrownparagraph"></p>
+        </div>
 
-      <div class="bg-white mt-3">
-        <form class="forminputpadding">
-          <div class="my-2">
-            <label for="" class="d-block medbrownparagraph graytext"
-              >Other Name
-              <span class="text-danger">*</span>
-            </label>
-            <input
-              type="text"
-              required
-              placeholder="e.g Nuella"
-              class="forminputs text-dark"
-            />
-          </div>
-          <div class="my-2">
-            <label for="" class="d-block medbrownparagraph graytext"
-              >Surname
-              <span class="text-danger">*</span>
-            </label>
-            <input
-              type="text"
-            
-              required
-              placeholder="e.g Ime"
-              class="forminputs text-dark"
-            />
-          </div>
-          <div class="my-2">
-            <label for="" class="d-block medbrownparagraph graytext"
-              >Headline
-              <span class="text-danger">*</span>
-            </label>
-            <input
-              type="text"
-              required
-              placeholder="e.g Student"
-              class="forminputs text-dark"
-            />
-          </div>
-          <div class="my-2">
-            <label for="" class="d-block medbrownparagraph graytext"
-              >Biography
-              <span class="text-danger">*</span>
-            </label>
-            <input
-              type="text"
-              required
-              placeholder="e.g Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
-              class="forminputs text-dark"
-            />
-          </div>
+        <div class="bg-white mt-3 d-flex justify-content-center">
+          <form
+            class="forminputpadding w-75 width75"
+            @submit.prevent="submitFunction"
+          >
+            <div class="my-4 py-2">
+              <label for="" class="d-block medbrownparagraph graytext"
+                >Surname
+                <span class="text-danger">*</span>
+              </label>
+              <input
+                type="text"
+                required
+                v-model="formInputs.surname"
+                placeholder="e.g Nuella"
+                class="forminputs text-dark"
+              />
+            </div>
+            <div class="my-4 py-2">
+              <label for="" class="d-block medbrownparagraph graytext"
+                >Registeration Number
+                <span class="text-danger">*</span>
+              </label>
+              <input
+                type="text"
+                required
+                v-model="formInputs.regNo"
+                placeholder="e.g Ime"
+                class="forminputs text-dark"
+              />
+            </div>
 
-          <!-- <div class="my-3">
-            <h2 class="text-center brown24">Links</h2>
-          </div> -->
-          <!-- 
-          <div class="my-2">
-            <label for="" class="d-block medbrownparagraph graytext"
-              >LinkedIn
-              <span class="text-danger">*</span>
-            </label>
-            <input
-              type="text"
-              name=""
-              id=""
-              required
-              placeholder="e.g https://www.linkedin.com/in/username/"
-              class="forminputs text-dark"
-            />
-          </div> -->
-
-          <div class="d-flex mt-3 justify-content-start pb-5">
-            <input
-              type="submit"
-              value="Save"
-              class="btn mainbtndashboard px-5 mb-5"
-            />
-          </div>
-        </form>
+            <div class="my-4 py-2 d-flex justify-content-center">
+              <button
+                class="
+                  btn
+                  py-md-3 py-2
+                  mainbtndashboard
+                  largebrownparagraph
+                  w-100
+                  text-center text
+                  bold700
+                "
+              >
+                Check in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -93,8 +70,41 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      formInputs: {
+        surname: '',
+        regNo: '',
+      },
+    }
+  },
+  methods: {
+    async submitFunction() {
+      try {
+        await this.$axios.$post('', {
+          school_id: process.env.SCHOOL_ID,
+          surname: this.formInputs.surname,
+          registation_number: this.formInputs.regNo,
+          course_id: this.$route.params.course,
+          event_id: this.$route.params.event,
+        })
+        this.$toast.success('Registration Successful')
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style  scoped>
+.width75 {
+  width: 75%;
+}
+
+@media (max-width: 991.5px) {
+  .width75 {
+    width: 85%;
+  }
+}
+</style>
