15c381aa86088a5e848c790f3c5389a4f1c3a3cb
happi2206 <happinessadeboye@gmail.com>
2022-02-25 15:00:51 +0100

    pdf qr download

---
+++ components/QRGenerator.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <client-only>
+      <vue-html2pdf
+        :show-layout="true"
+        :float-layout="true"
+        :enable-download="true"
+        :preview-modal="true"
+        :paginate-elements-by-height="1400"
+        filename="Attendance QR code"
+        :pdf-quality="2"
+        :manual-pagination="false"
+        pdf-format="a4"
+        pdf-orientation="portrait"
+        pdf-content-width="800px"
+        @hasStartedGeneration="hasStartedGeneration()"
+        @hasGenerated="hasGenerated($event)"
+        ref="html2Pdf"
+      >
+        <section slot="pdf-content">
+          <section class="intro">
+            <h1>
+              ATTENDANCE of
+
+              <span> {{ eventData.name }}</span>
+            </h1>
+            <h2>CLASS OF 2022</h2>
+
+            <div class="d-flex justify-content-between mx-5 px-5">
+              <p class="text-center">
+                Start Date: {{ eventData.start_date.slice(0, 10) }}
+              </p>
+              <p class="text-center">
+                End Date: {{ eventData.end_date.slice(0, 10) }}
+              </p>
+            </div>
+            <!-- place-holder image -->
+            <figure class="d-flex justify-content-center align-items-center">
+              <VueQRCodeComponent
+                text="https://github.com/gerardreches/vue-qrcode-component"
+              ></VueQRCodeComponent>
+            </figure>
+            <h3>SCAN TO CHECK IN</h3>
+            <div class="container">
+              <ol>
+                <li>Open your phone camera and point at the picture.</li>
+                <li class="d-flex align-items-center">
+                  <span>
+                    Once focued on the picture it will show Koinonia or a</span
+                  >
+
+                  <span
+                    class="iconify mt-4"
+                    data-icon="bx:barcode-reader"
+                  ></span>
+
+                  icon.
+                </li>
+                <li>
+                  Tap on the name or the icon to open the attendance web page.
+                </li>
+                <li>
+                  Fill in your Surname and your 3 digit Registration Number on
+                  the spaces provided.
+                </li>
+                <li>Click on Check in button.</li>
+              </ol>
+              <p class="stand-out">
+                Note: If your camera does not pick up the QR code, you can
+                download a free QR code scanner from App Store or Play Store.
+              </p>
+
+              <p>
+                *You would need to have data on your phone to self check in.
+              </p>
+            </div>
+          </section>
+        </section>
+      </vue-html2pdf>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import VueHtml2pdf from 'vue-html2pdf'
+import VueQRCodeComponent from 'vue-qrcode-component'
+
+export default {
+  props: {
+    linkCode: {
+      type: String,
+    },
+
+    eventData: {
+      type: Object,
+    },
+  },
+  methods: {
+    generateReport() {
+      this.$refs.html2Pdf.generatePdf()
+    },
+  },
+  mounted() {
+    console.log('mounted qrcode')
+  },
+
+  components: {
+    VueHtml2pdf,
+    VueQRCodeComponent,
+  },
+}
+</script>
+
+<style scoped>
+.intro h1,
+h2,
+h3 {
+  text-align: center;
+}
+​ .intro h1 {
+  font-size: 3rem;
+  margin-top: 0rem;
+}
+​ .intro h2 {
+  font-size: 2rem;
+  margin: 0;
+  padding: 0;
+}
+​ .intro h3 {
+  font-size: 1.5rem;
+  font-weight: bolder;
+  margin: 0;
+}
+​ .intro p {
+  font-size: 1.3rem;
+  font-weight: 900;
+  color: #fca311;
+}
+​ ol {
+  padding: 0.8rem 1.4rem 1.4rem 1.4rem;
+  margin: 0;
+}
+​ .intro li {
+  font-size: 1.3rem;
+  padding: 0.5rem 0;
+  font-weight: 500;
+}
+​ .container {
+  padding-left: 3rem;
+}
+​ .container img {
+  height: 16px;
+  width: 16px;
+}
+​ figure {
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+  flex-shrink: 2;
+}
+​ .intro img {
+  width: 50%;
+  height: 50%;
+}
+​ .stand-out {
+  margin-top: 0;
+}
+​ p:last-child {
+  margin-bottom: 3rem;
+}
+​ .icon {
+  position: relative;
+  top: 3px;
+}
+</style>
