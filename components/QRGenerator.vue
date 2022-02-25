3f5d50c8f7bd85e000e19e01ed33bd0eef65186c
Dimeji Ogunleye <dimeji.ogunleye20@gmail.com>
2022-02-25 17:20:35 +0100

    Updated QRscanner Layout

--- components/QRGenerator.vue
+++ components/QRGenerator.vue
@@ -10,28 +10,29 @@
         filename="Attendance QR code"
         :pdf-quality="2"
         :manual-pagination="false"
-        pdf-format="a4"
+        pdf-format="A4"
         pdf-orientation="portrait"
-        pdf-content-width="800px"
+        pdf-content-width="100%"
         @hasStartedGeneration="hasStartedGeneration()"
         @hasGenerated="hasGenerated($event)"
         ref="html2Pdf"
       >
         <section slot="pdf-content">
           <section class="intro">
-            <h3 class="text-uppercase mt-4">
-              <span> {{ eventData.name }}</span> ATTENDANCE
-            </h3>
+            <h1 class="text-uppercase mt-6 mb-4">ATTENDANCE</h1>
+
+            <h3 class="text-uppercase mt-4 mb-4">{{ eventData.name }}</h3>
             <!-- <h3>CLASS OF 2022</h3> -->
 
-            <div class="d-flex justify-content-center mx-5 px-5 my-3">
+            <!-- <div class="d-flex justify-content-center mx-5 px-5 my-3">
               <h4 class="text-center">
                 {{ eventData.start_date | DateTimeFormat }}
               </h4>
-            </div>
+            </div> -->
             <!-- place-holder image -->
             <figure
               class="d-flex justify-content-center align-items-center my-5"
+              style="width: 100%; height: 100%"
             >
               <VueQRCodeComponent
                 text="https://koinonia-nuxt.onrender.com/checkIn/course/62145ac6ab45aafcabd7da17/event/62163631433c93e04a960c5d"
@@ -39,12 +40,18 @@
             </figure>
             <h3 class="mb-3">SCAN TO CHECK IN</h3>
             <div class="container">
-              <ol>
-                <li>1. Open your phone camera and point at the picture.</li>
-                <li class="d-flex align-items-center">
-                  2.
-                  <span>
-                    Once focued on the picture it will show Koinonia or a</span
+              <ol style="margin-left: 2.5rem">
+                <li style="font-size: 1rem; padding: 1rem 0">
+                  1. Open your phone camera and point at the picture.
+                </li>
+                <li
+                  style="font-size: 1rem; padding: 1rem 0"
+                  class="d-flex align-items-center"
+                >
+                  2.&nbsp;
+                  <span
+                    >Once focused on the picture it will show
+                    <b><i>Koinonia</i></b> or a</span
                   >
 
                   <div class="pt-3 mx-2">
@@ -53,24 +60,30 @@
 
                   icon.
                 </li>
-                <li>
+                <li style="font-size: 1rem; padding: 1rem 0">
                   3. Tap on the name or the icon to open the attendance web
                   page.
                 </li>
-                <li>
+                <li style="font-size: 1rem; padding: 1rem 0">
                   4. Fill in your Surname and your 3 digit Registration Number
                   on the spaces provided.
                 </li>
-                <li>5. Click on Check in button.</li>
+                <li style="font-size: 1rem; padding: 1rem 0">
+                  5. Click on Check in button.
+                </li>
               </ol>
-              <p class="stand-out">
-                Note: If your camera does not pick up the QR code, you can
-                download a free QR code scanner from App Store or Play Store.
-              </p>
+              <b
+                ><p style="margin-left: 2.5rem" class="stand-out">
+                  Note: If your camera does not pick up the QR code, you can
+                  download a free QR code scanner from App Store or Play Store.
+                </p></b
+              >
 
-              <p>
-                *You would need to have data on your phone to self check in.
-              </p>
+              <b
+                ><p style="margin-left: 2.5rem">
+                  *You would need to have data on your phone to self check in.
+                </p></b
+              >
             </div>
           </section>
         </section>
@@ -117,7 +130,7 @@ h3 {
 }
 ​ .intro h1 {
   font-size: 3rem;
-  margin-top: 0rem;
+  margin: 2rem 0;
 }
 ​ .intro h2 {
   font-size: 2rem;
@@ -138,11 +151,11 @@ h3 {
   padding: 0.8rem 1.4rem 1.4rem 1.4rem;
   margin: 0;
 }
-​ .intro li {
-  font-size: 1.3rem;
-  padding: 0.5rem 0;
-  font-weight: 500;
-}
+/* ​ .intro li {
+  font-size: 1.5rem;
+  padding: 2rem 0;
+  
+} */
 ​ .container {
   padding-left: 3rem;
 }
@@ -158,8 +171,8 @@ h3 {
   flex-shrink: 2;
 }
 ​ .intro img {
-  width: 50%;
-  height: 50%;
+  width: 50vw;
+  height: 50vh;
 }
 ​ .stand-out {
   margin-top: 0;
